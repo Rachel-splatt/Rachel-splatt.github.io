cb107a4083e9b04c61c0d3f1a4c23bc4ceb080c7
Rachel Platt <rachel.s.platt@colorado.edu>
2018-01-20 17:32:03 -0700

    major changes hip hip hooray

---
+++ js/typing.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var typed = new Typed('#typed', {
+    stringsElement: '#typed-strings',
+    typeSpeed: 20,
+    backSpeed: 20,
+    startDelay: 1000,
+    loop: false,
+    loopCount: Infinity,
+      contentType:'html',
+      
+      
+
+  });
+    
+    
+});
